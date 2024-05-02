9e21f0230051c47f7710c01781a072c1ec1a57a6
pulnip <choiwh1008@gmail.com>
2024-05-02 10:56:00 +0900

    split VDB

--- commons/ClientWrapper.cs
+++ commons/ClientWrapper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using commons.Table;
 
-namespace commons
+namespace commons.VirtualDB
 {
     public abstract class VirtualDatabase<Key, T>
     {
@@ -23,54 +23,6 @@ namespace commons
         public abstract bool sync();
     };
     
-    public class ScheduleVDB: VirtualDatabase<string, ScheduleInfo>
-    {
-        public override bool create(in ScheduleInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool createItems(in ScheduleInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override ScheduleInfo read(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
-        public ScheduleInfo[] readFromDate(in string date)
-        {
-            throw new NotImplementedException();
-        }
-        public override ScheduleInfo[] readItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool update(in ScheduleInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool updateItems(in ScheduleInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool delete(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
-        public bool deleteFromDate(in string date)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool deleteItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool sync()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public static class ClientWrapper
     {
         public static bool createMemberInfo(in MemberInfo newMember)
---
+++ commons/ScheduleVDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using commons.Table;
+
+namespace commons.VirtualDB
+{
+    public class ScheduleVDB : VirtualDatabase<string, ScheduleInfo>
+    {
+        public override bool create(in ScheduleInfo item)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool createItems(in ScheduleInfo[] items)
+        {
+            throw new NotImplementedException();
+        }
+        public override ScheduleInfo read(in string scheduleId)
+        {
+            throw new NotImplementedException();
+        }
+        public ScheduleInfo[] readFromDate(in string date)
+        {
+            throw new NotImplementedException();
+        }
+        public override ScheduleInfo[] readItems(in string[] ids)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool update(in ScheduleInfo item)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool updateItems(in ScheduleInfo[] items)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool delete(in string scheduleId)
+        {
+            throw new NotImplementedException();
+        }
+        public bool deleteFromDate(in string date)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool deleteItems(in string[] ids)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool sync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
