04fa7d6fa5d378e58226f3b2c6f3464114c97aad
pulnip <choiwh1008@gmail.com>
2024-05-03 21:31:18 +0900

    create specific vdb interface

--- commons/LoginVDB.cs
+++ commons/LoginVDB.cs
@@ -8,43 +8,5 @@ using commons.Table;
 
 namespace commons.VirtualDB
 {
-    public class LoginVDB : VirtualDatabase<string, LoginInfo>
-    {
-        public override bool create(in LoginInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool createItems(in LoginInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override LoginInfo read(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
-        public override LoginInfo[] readItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool update(in LoginInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool updateItems(in LoginInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool delete(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool deleteItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool sync()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    public class LoginVDB : VirtualDatabase<string, LoginInfo>{ }
 }
--- commons/ScheduleVDB.cs
+++ commons/ScheduleVDB.cs
@@ -10,47 +10,11 @@ namespace commons.VirtualDB
 {
     public class ScheduleVDB : VirtualDatabase<string, ScheduleInfo>
     {
-        public override bool create(in ScheduleInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool createItems(in ScheduleInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override ScheduleInfo read(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
         public ScheduleInfo[] readFromDate(in string date)
         {
             throw new NotImplementedException();
         }
-        public override ScheduleInfo[] readItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool update(in ScheduleInfo item)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool updateItems(in ScheduleInfo[] items)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool delete(in string scheduleId)
-        {
-            throw new NotImplementedException();
-        }
         public bool deleteFromDate(in string date)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool deleteItems(in string[] ids)
-        {
-            throw new NotImplementedException();
-        }
-        public override bool sync()
         {
             throw new NotImplementedException();
         }
