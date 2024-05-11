0dac23a9c147eac6355593f76561a4c32c147f12
pogery_foggy <s2k616@gmail.com>
2024-05-11 19:31:03 +0900

    error fix

--- client/Form1.Designer.cs
+++ client/Form1.Designer.cs
@@ -158,7 +158,6 @@
             this.Controls.Add(this.LoginUIidtxtBox);
             this.Controls.Add(this.LoginUIsigninBtn);
             this.Controls.Add(this.button1);
-            this.Contrors.Add(this.btnTmp);
             this.Controls.Add(this.btnMain);
             this.Controls.Add(this.button2);
             this.Margin = new System.Windows.Forms.Padding(4);
